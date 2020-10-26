cb4d90eab100ba71aeacf3bd4796de15d0c6d9bb
Jared Corker <jared.corker@gmail.com>
2020-10-26 12:41:33 +1100

    Rotation now works

--- enigma.py
+++ enigma.py
@@ -1,25 +1,25 @@
 # Initial Test Version of Enigma
 
-alphabet = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
-
 class Rotor():
     def __init__(self, wiring, position):
+        self.alphabet = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
         self.wiring = wiring
         self.position = position
         self.reversed = wiring[::-1]
 
     def rotate(self):
+        self.alphabet = [self.alphabet[-1]] + self.alphabet[:-1]
         self.wiring = [self.wiring[-1]] + self.wiring[:-1]
         self.position += 1
         if self.position == 27:
             self.position = 1
 
-    def return_letter(self, letter):
-        return self.wiring[alphabet.index(letter)]
+    def return_char(self, char):
+        return self.wiring[self.alphabet.index(char)]
     
-    def return_letter_reverse(self, letter):
-        index = self.wiring.index(letter)
-        return alphabet[index] 
+    def return_char_reverse(self, char):
+        index = self.wiring.index(char)
+        return self.alphabet[index] 
 
     def reset_position(self):
         while(self.position != 1):
@@ -33,19 +33,28 @@ class Enigma():
         self.rotor_three = rotor_three
         self.reflector = reflector
 
+    def process_char(self, char):
+            char = rotor_one.return_char(char)
+            char = rotor_two.return_char(char)
+            char = rotor_three.return_char(char)
+            char = reflector.return_char(char)
+            char = rotor_three.return_char_reverse(char)
+            char = rotor_two.return_char_reverse(char)
+            char = rotor_one.return_char_reverse(char)
+
+            self.rotor_one.rotate()
+            if self.rotor_one.position == 1:
+                self.rotor_two.rotate()
+                if self.rotor_two.position == 1:
+                    self.rotor_three.rotate()
+
+            return char
+
     def encrypt(self, message):
         output = ""
         message = message.upper()
         for char in message:
-            char = rotor_one.return_letter(char)
-            char = rotor_two.return_letter(char)
-            char = rotor_three.return_letter(char)
-            char = reflector.return_letter(char)
-            char = rotor_three.return_letter_reverse(char)
-            char = rotor_two.return_letter_reverse(char)
-            char = rotor_one.return_letter_reverse(char)
-            output += char
-
+            output += self.process_char(char)
         return output
 
 rotor_one = Rotor(list('EKMFLGDQVZNTOWYHXUSPAIBRCJ'), 1)
@@ -53,6 +62,6 @@ rotor_two = Rotor(list('AJDKSIRUXBLHWTMCQGZNPYFVOE'), 1)
 rotor_three = Rotor(list('BDFHJLCPRTXVZNYEIWGAKMUSQO'), 1)
 reflector = Rotor(list('EJMZALYXVBWFCRQUONTSPIKHGD'), 1)
 enig = Enigma(rotor_one, rotor_two, rotor_three, reflector)
-message = enig.encrypt("Hello")
-print(enig.encrypt("Hello"))
+message = enig.encrypt("AAA")
+print(enig.encrypt("AAA"))
 print(enig.encrypt(message))
